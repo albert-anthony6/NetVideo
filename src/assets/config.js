611868b06e3e657317a7fa65feab575e4eb9f6f8
Albert Anthony <albert.anthony1999@gmail.com>
2020-04-18 21:41:24 -0500

    Added key variable

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,6 +1,6 @@
 // Configuration for NetVideo
 
 const API_URL = 'https://www.googleapis.com/youtube/v3/';
-const API_KEY = 'Hidden';
+const API_KEY = process.env.REACT_APP_API_KEY; //Hidden
 
 export { API_URL, API_KEY };
